b06118f52e62a5a25cd5c1d7fdd54900cd2a3f63
superdreams <superdreams@gmail.com>
2022-04-05 17:28:24 +0800

    Update config.php
    config cache  prefix

--- app/config.php
+++ app/config.php
@@ -223,7 +223,7 @@ return [
         // 缓存保存目录
         'path'   => CACHE_PATH,
         // 缓存前缀
-        'prefix' => 'finddata',
+        'prefix' => 'finddata_',
         // 缓存有效期 0表示永久缓存
         'expire' => 0,
     ],
